5ce3e55da0dc973ac936330c9ef04848d0a5badb
emrutsdmr <emrullahtsdmr@gmail.com>
2025-07-12 13:51:37 +0200

    Make number and publish period configurable via parameters

--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -2,13 +2,22 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
+from rclpy.parameter import Parameter
 
 class NumberPublisherNode(Node):
   def __init__(self):
     super().__init__("number_publisher")
-    self.number_ = 2
+
+    self.declare_parameter("number", 2)
+    self.declare_parameter("publish_period", 1.0)
+
+    self.number_ = self.get_parameter("number").value
+    self.timer_period_ = self.get_parameter("publish_period").value
+
+    self.add_post_set_parameters_callback(self.parameters_callback)
+
     self.number_publisher_ = self.create_publisher(Int64, "number", 10)
-    self.number_timer_ = self.create_timer(1.0, self.publish_number)
+    self.number_timer_ = self.create_timer(self.timer_period_, self.publish_number)
     self.get_logger().info("Number publisher has been started.")
 
   def publish_number(self):
@@ -16,11 +25,18 @@ class NumberPublisherNode(Node):
     msg.data = self.number_
     self.number_publisher_.publish(msg)
 
+  def parameters_callback(self, params: list[Parameter]):
+    for param in params:
+      if param.name == "number":
+        self.number_ = param.value
+
+
 def main(args=None):
   rclpy.init(args=args)
   node = NumberPublisherNode()
   rclpy.spin(node)
   rclpy.shutdown()
+
 
 if __name__ == "__main__":
   main()
